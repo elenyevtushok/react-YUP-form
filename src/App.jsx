ec5385aeb378c37f36862371309430524438a2fb
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2023-02-24 11:20:04 +0100

    added feature b

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { FeatureB } from './components/FeatureB';
 import { Form } from './components/Form'
 
 function App() {
 	return(
 		<div className='App'>
 			<Form />
+			<FeatureB />
 		</div>
 	)
 }
